68ece65bb223bad76e8662d373421cb10b9abf8a
JellyLu <yjlu@thoughtworks.com>
2019-12-30 23:04:22 +0800

    feat(spike charts): add pie chart for Chart Kit Charts

--- src/charts/ChartKitCharts.tsx
+++ src/charts/ChartKitCharts.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
-import { BarChart, LineChart, ProgressChart } from "react-native-chart-kit";
+import { BarChart, LineChart, PieChart, ProgressChart } from "react-native-chart-kit";
 import { SIZE } from "../../src/config/size"
 import { blue, lightGrey } from "../../src/config/color"
 
@@ -59,6 +59,44 @@ const progressChartData = {
   data: [0.4, 0.6, 0.8]
 }
 
+const pieChartData = [
+  {
+    name: "Seoul",
+    population: 21500000,
+    color: "rgba(131, 167, 234, 1)",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 12
+  },
+  {
+    name: "Toronto",
+    population: 2800000,
+    color: "#F00",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 12
+  },
+  {
+    name: "Beijing",
+    population: 527612,
+    color: "red",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 12
+  },
+  {
+    name: "New York",
+    population: 8538000,
+    color: "#ffffff",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 12
+  },
+  {
+    name: "Moscow",
+    population: 11920000,
+    color: "rgb(0, 0, 255)",
+    legendFontColor: "#7F7F7F",
+    legendFontSize: 12
+  }
+];
+
 class ChartKitCharts extends React.Component {
   static navigationOptions = {
     title: 'Bar Chart'
@@ -95,6 +133,19 @@ class ChartKitCharts extends React.Component {
           height={260}
           chartConfig={chartConfig}
           hideLegend={false}
+        />
+        <View style={styles.title}><Text style={styles.text}>Pie Chart</Text></View>
+        <PieChart
+          style={styles.graphStyle}
+          data={pieChartData}
+          width={width}
+          height={260}
+          chartConfig={chartConfig}
+          accessor="population"
+          backgroundColor="#022173"
+          paddingLeft={15}
+          absolute
+          bgColor="#022173"
         />
       </ScrollView>
     );
