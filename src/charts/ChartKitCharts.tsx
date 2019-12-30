5f3f83640cd809057a917da2ae2c5e4a0afda467
JellyLu <yjlu@thoughtworks.com>
2019-12-30 10:27:10 +0800

    feat(spike charts): add bar chart of Chart Kit Charts

--- src/charts/ChartKitCharts.tsx
+++ src/charts/ChartKitCharts.tsx
@@ -1,19 +1,58 @@
 import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { BarChart } from "react-native-chart-kit";
+import { SIZE } from "../../src/config/size"
+import { blue, lightGrey } from "../../src/config/color"
+
+const chartConfig = {
+backgroundColor: '#022173',
+  backgroundGradientFrom: '#022173',
+  backgroundGradientTo: '#1b3fa0',
+  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+  strokeWidth: 4,
+  barPercentage: 0.5
+};
+
+const barChartData = {
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      data: [20, 45, 28, 80, 99, 43]
+    }
+  ]
+};
+
 class ChartKitCharts extends React.Component {
   static navigationOptions = {
     title: 'Bar Chart'
   };
 
   public render() {
+  const width = SIZE.MAX_WIDTH - 20;
     return (
     <ScrollView>
       <View style={styles.title}><Text style={styles.text}>Bar Chart</Text></View>
+      <BarChart
+        style={styles.graphStyle}
+        data={barChartData}
+        width={width}
+        height={260}
+        yAxisLabel={'$'}
+        chartConfig={chartConfig}
+        verticalLabelRotation={20}
+      />
     </ScrollView>
     );
   }
 }
+
 const styles = StyleSheet.create({
+  graphStyle: {
+    marginVertical: 8,
+    borderRadius: 16,
+    marginLeft: 10,
+    marginHorizontal: 10
+  },
   title: {
     height: 44,
     alignItems: 'center',
--- src/config/color.ts
+++ src/config/color.ts
@@ -3,3 +3,4 @@ export const blue = "#009edb";
 export const black = "#000";
 export const white = "#fff";
 export const red = "#f4511e";
+export const lightGrey = "#f2f8fa";
