83a6e033aa3c0d5f74306ff4bcf85f56d9e1afbf
JellyLu <yjlu@thoughtworks.com>
2020-01-07 14:32:22 +0800

    feat(spike charts): add average line

--- src/charts/SvgCharts.tsx
+++ src/charts/SvgCharts.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
-import { Grid, LineChart, XAxis, YAxis } from "react-native-svg-charts";
-import { Circle, Rect } from "react-native-svg";
+import { LineChart, XAxis, YAxis } from "react-native-svg-charts";
+import { Circle, Line, Rect } from "react-native-svg";
 import { SIZE } from "../config/size";
 
 const width = SIZE.MAX_WIDTH - 20;
@@ -22,18 +22,29 @@ class SvgCharts extends React.Component {
   public render() {
     const contentInset = { top: 10, bottom: 10, left: 10, right: 10 };
     const axesSvg = { fontSize: 10, fill: 'grey' };
-    const Decorator = ({ x, y, data }: any) => {
-      return data.map((value: any, index: any) => (
-        <Circle
-          key={index}
-          cx={x(index)}
-          cy={y(value)}
-          r={4}
-          stroke={'rgb(134, 65, 244)'}
-          fill={'white'}
-        />
-      ))
-    };
+    const Decorator = ({ x, y, data }: any) => data.map((value: any, index: any) => (
+      <Circle
+        key={index}
+        cx={x(index)}
+        cy={y(value)}
+        r={4}
+        stroke={'rgb(134, 65, 244)'}
+        fill={'white'}
+      />
+    ));
+
+    const HorizontalLine = (({ y }: any) => (
+      <Line
+        key={'zero-axis'}
+        x1={'0%'}
+        x2={'100%'}
+        y1={y(60)}
+        y2={y(60)}
+        stroke={'grey'}
+        strokeDasharray={[4, 8]}
+        strokeWidth={2}
+      />
+    ))
 
     return (
       <ScrollView>
@@ -52,7 +63,7 @@ class SvgCharts extends React.Component {
               svg={{ stroke: 'rgb(134, 65, 244)' }}
               contentInset={contentInset}
             >
-              <Grid />
+              <HorizontalLine />
               <Decorator />
             </LineChart>
             <XAxis
