4b37429dc8c2f1ba5163d5e7be31bce17046fbda
JellyLu <yjlu@thoughtworks.com>
2020-01-15 10:49:56 +0800

    feat(firebase spike): revert firebase related feature, will use firebase-react-native to replace

--- ios/phoenix/AppDelegate.m
+++ ios/phoenix/AppDelegate.m
@@ -11,7 +11,6 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
-@import Firebase;
 
 @implementation AppDelegate
 
@@ -29,7 +28,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  [FIRApp configure];
+
   return YES;
 }
 
