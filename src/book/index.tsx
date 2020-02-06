3dcdc073eb1d30189fd227d2be3013bd5ba30d67
Shane <xinzhang@thoughtworks.com>
2020-02-06 15:54:07 +0800

    update the style for books

--- src/book/index.tsx
+++ src/book/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
-import { ImageBackground, Text, StyleSheet, View } from 'react-native';
+import { ImageBackground, Text, StyleSheet, View, Image } from 'react-native';
 import { onProductView } from '../utils/analyticsUtils';
 import { lightPurpe } from 'src/config/color';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bookrack = require('../../assets/bookrack.jpg');
+const books = [
+  { uri: 'https://facebook.github.io/react-native/img/tiny_logo.png' },
+  { uri: 'https://facebook.github.io/react-native/img/tiny_logo.png' },
+  { uri: 'https://facebook.github.io/react-native/img/tiny_logo.png' },
+  { uri: 'https://facebook.github.io/react-native/img/tiny_logo.png' },
+  { uri: 'https://facebook.github.io/react-native/img/tiny_logo.png' },
+  { uri: 'https://facebook.github.io/react-native/img/tiny_logo.png' },
+  { uri: 'https://facebook.github.io/react-native/img/tiny_logo.png' },
+];
 
 class Book extends React.Component {
   static navigationOptions = {
@@ -12,13 +24,24 @@ class Book extends React.Component {
     onProductView();
   }
 
+  private chooseBook() {
+    console.log('**********');
+  }
+
   public render() {
     return (
-      <ImageBackground
-        source={require('../../assets/bookrack.jpg')}
-        style={styles.background}>
+      <ImageBackground source={bookrack} style={styles.background}>
         <View style={styles.container}>
           <Text style={styles.title}>我的书架</Text>
+          <View style={styles.books}>
+            {books.map((book, index) => (
+              <Image
+                key={index}
+                source={{ uri: book.uri }}
+                style={styles.book}
+              />
+            ))}
+          </View>
         </View>
       </ImageBackground>
     );
@@ -32,13 +55,26 @@ const styles = StyleSheet.create({
   },
   container: {
     display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    paddingLeft: 5,
+    paddingRight: 5,
   },
   title: {
-    paddingTop: 15,
+    marginTop: 15,
+    marginBottom: 20,
     fontSize: 20,
     color: lightPurpe,
+    alignSelf: 'center',
+  },
+  books: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  book: {
+    width: 110,
+    height: 180,
+    marginBottom: 63,
   },
 });
 
