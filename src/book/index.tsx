5800c8c79f3407a362b7e136b9c168b983b6c486
Shane <xinzhang@thoughtworks.com>
2020-02-06 15:03:55 +0800

    add bookrack

--- src/book/index.tsx
+++ src/book/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ImageBackground, Text, StyleSheet, View } from 'react-native';
 import { onProductView } from '../utils/analyticsUtils';
+import { lightPurpe } from 'src/config/color';
 
 class Book extends React.Component {
   static navigationOptions = {
@@ -13,11 +14,32 @@ class Book extends React.Component {
 
   public render() {
     return (
-      <View>
-        <Text>Hello Book Page</Text>
-      </View>
+      <ImageBackground
+        source={require('../../assets/bookrack.jpg')}
+        style={styles.background}>
+        <View style={styles.container}>
+          <Text style={styles.title}>我的书架</Text>
+        </View>
+      </ImageBackground>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  background: {
+    width: '100%',
+    height: '100%',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  title: {
+    paddingTop: 15,
+    fontSize: 20,
+    color: lightPurpe,
+  },
+});
 
 export default Book;
